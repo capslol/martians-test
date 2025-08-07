a3f1206cd64f677d576a0809f630e5e695af3bbb
capslol <maxim.balov19@gmail.com>
2025-08-07 16:56:40 +0200

    Добавлен файл vercel.json для настройки перенаправлений в Vite, а также удалены устаревшие комментарии в конфигурации Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // В современных версиях Vite SPA роутинг работает автоматически в dev режиме
-  // Для production используем vercel.json
+  
 })
